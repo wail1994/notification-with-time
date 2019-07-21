ba37a31575546e6de65f623a5b118c10e2ab6379
wail1994 <capsssss@hotmail.com>
2019-07-21 19:49:10 +0300

    Add files via upload

---
+++ MainActivity.cs
@@ -0,0 +1,91 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Support.V4.App;
+using Android.Content;
+using Java.Util;
+using System;
+using System.Runtime.Remoting.Contexts;
+using Context = System.Runtime.Remoting.Contexts.Context;
+using Java.Lang;
+
+namespace App1
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        Button d;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            d = FindViewById<Button>(Resource.Id.button1);
+            d.Click += delegate
+            {
+
+                //set the current time and date for this calendar
+                Java.Util.Calendar calendar = Java.Util.Calendar.Instance;
+              
+                calendar.Set(CalendarField.Hour, 5);
+                calendar.Set(CalendarField.Minute, 21);
+
+                var alarmIntent = new Intent(this, typeof(NotificationPublisher));
+                AlarmManager alarmManager = (AlarmManager)GetSystemService(AlarmService);
+                var pending = PendingIntent.GetBroadcast(this, 0, alarmIntent, PendingIntentFlags.UpdateCurrent);
+               alarmManager = GetSystemService(AlarmService).JavaCast<AlarmManager>();
+                alarmManager.SetRepeating(AlarmType.Rtc, calendar.TimeInMillis,
+        AlarmManager.IntervalHour, pending);
+
+                Toast.MakeText(this, "Alarm set In: " + d + " seconds", ToastLength.Short).Show();
+            };
+
+        }
+        long GetDateTimeMS()
+        {
+            Java.Util.Calendar calendar = Java.Util.Calendar.Instance;
+            calendar.TimeInMillis = Java.Lang.JavaSystem.CurrentTimeMillis();
+            calendar.Set(CalendarField.Hour, 4);
+            calendar.Set(CalendarField.Minute, 16);
+            return calendar.TimeInMillis;
+        }
+
+        public void TimeSelectOnClick(object sender, EventArgs eventArgs)
+        {
+            TimePickerFragment frag = TimePickerFragment.NewInstance(
+                delegate (DateTime time)
+                {
+                    int d = time.Hour;
+
+                    long m = time.Minute;
+                });
+
+            frag.Show(FragmentManager, TimePickerFragment.TAG);
+        }
+
+        public void f() {
+
+
+
+
+
+        } 
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+}
---
+++ NotificationPublisher.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Media;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V4.App;
+using Android.Views;
+using Android.Widget;
+
+namespace App1
+{
+    [BroadcastReceiver]
+    public class NotificationPublisher : BroadcastReceiver
+    {
+        
+       
+           public const string URGENT_CHANNEL = "com.xamarin.myapp.urgent";
+        public const int NOTIFY_ID = 1100;
+        public override void OnReceive(Android.Content.Context context, Intent intent)
+        {
+            var message = intent.GetStringExtra("message");
+            var title = intent.GetStringExtra("title");
+
+            var importance = NotificationImportance.High;
+            NotificationChannel chan = new NotificationChannel(URGENT_CHANNEL, "Urgent", importance);
+            chan.EnableVibration(true);
+            chan.LockscreenVisibility = NotificationVisibility.Public;
+
+
+
+            var resultIntent = new Intent(context, typeof(MainActivity));
+            intent.AddFlags(ActivityFlags.ClearTop);
+            var pendingIntent = PendingIntent.GetActivity(context, 0, resultIntent, PendingIntentFlags.UpdateCurrent);
+
+            var notificationBuilder = new NotificationCompat.Builder(context)
+                .SetSmallIcon(Resource.Drawable.worldtem)
+                .SetContentTitle(title)
+                .SetContentText(message)
+                .SetContentIntent(pendingIntent)
+                .SetSound(RingtoneManager.GetDefaultUri(RingtoneType.Notification))
+                .SetAutoCancel(true)
+                .SetChannelId(URGENT_CHANNEL);
+
+            NotificationManager notificationManager = (NotificationManager)context.GetSystemService(Android.Content.Context.NotificationService);
+            notificationManager.CreateNotificationChannel(chan);
+
+            notificationManager.Notify(NOTIFY_ID, notificationBuilder.Build());
+
+            //
+
+
+        }
+
+    }
+}
